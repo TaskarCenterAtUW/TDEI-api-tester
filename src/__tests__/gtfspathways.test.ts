342f437c0f6f3eb695eef00b56185dfe9f410e1b
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-04-14 18:21:15 +0530

    removing pathways upload as it is conflicting

--- src/__tests__/gtfspathways.test.ts
+++ src/__tests__/gtfspathways.test.ts
@@ -46,42 +46,5 @@ describe('GTFS PATHWAYS API', () => {
         console.log(versions.data.versions);
         expect(Array.isArray(versions.data.versions)).toBe(true);
 
-    }, 10000)
-
-    it("Should be able to upload Pathways files", async () => {
-        let metaToUpload = Utility.getRandomGtfsPathwaysUpload();
-        const requestInterceptor = (request: InternalAxiosRequestConfig, fileName: string) => {
-            if (
-                request.url === `${configuration.basePath}/api/v1/gtfs-pathways`
-            ) {
-                let data = request.data as FormData;
-                let file = data.get("file") as File;
-                delete data["file"];
-                delete data["meta"];
-                data.set("file", file, fileName);
-                data.set("meta", JSON.stringify(metaToUpload));
-            }
-            return request;
-        };
-        // Actual method does not give the results as expected
-        // So we are writing interceptor
-        const uploadInterceptor = axios.interceptors.request.use((req: InternalAxiosRequestConfig) =>
-            requestInterceptor(req, "pathways-test-upload.zip")
-        );
-        let fileDir = path.dirname(path.dirname(__dirname));
-        let payloadFilePath = path.join(
-            fileDir,
-            "assets/payloads/gtfs-pathways/files/success_1_all_attrs.zip"
-        );
-        let filestream = fs.readFileSync(payloadFilePath);
-        const blob = new Blob([filestream], { type: "application/zip" });
-        let pathwaysApi = new GTFSPathwaysApi(configuration);
-
-        const uploadedFileResponse = await pathwaysApi.uploadPathwaysFileForm(
-            metaToUpload,
-            blob
-        );
-        expect(uploadedFileResponse.data != "").toBe(true);
-
-    }, 50000);
+    }, 10000);
 });
