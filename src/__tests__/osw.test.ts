da17775319ecb1f041457ac6f984d9b9125c8f77
Sreekanth Ayydevara <sayydevara@gmail.com>
2023-04-14 00:24:52 -0700

    Minor refactoring and changed the test patterns to for better readability
    Given
    
    When
    
    Then

--- src/__tests__/osw.test.ts
+++ src/__tests__/osw.test.ts
@@ -5,81 +5,80 @@ import { Utility } from "../utils";
 import path from "path";
 import * as fs from "fs";
 
-describe('Tests for OSW', ()=>{
+describe("Tests for OSW", () => {
+  let configuration = new Configuration({
+    username: testHarness.system.username,
+    password: testHarness.system.password,
+    basePath: testHarness.system.baseUrl
+  });
 
-    let configuration = new Configuration({
-        username: testHarness.system.username,
-        password: testHarness.system.password,
-        basePath: testHarness.system.baseUrl
-        
+  beforeAll(async () => {
+    let generalAPI = new GeneralApi(configuration);
+    const loginResponse = await generalAPI.authenticate({
+      username: configuration.username,
+      password: configuration.password
     });
-    
-    beforeAll( async ()=>{
-        // console.log('Hello there');
-        let generalAPI = new GeneralApi(configuration);
-        const loginResponse = await generalAPI.authenticate({username:configuration.username,password:configuration.password});
-        configuration.baseOptions = {
-            headers:{
-                'Authorization':'Bearer '+ loginResponse.data.access_token
-            }
-        };
-        
-      
-    });
-    
-    it('Should get list of osw versions', async ()=>{
-        let oswAPI = new OSWApi(configuration);
-        const oswVersions = await oswAPI.listOswVersions();
-        expect(oswVersions.status).toBe(200);
-        expect(Array.isArray(oswVersions.data.versions)).toBe(true);
+    configuration.baseOptions = {
+      headers: {
+        Authorization: `Bearer ${loginResponse.data.access_token}`
+      }
+    };
+  });
 
-    });
-    
-    it('Should return list of osw files', async ()=>{
-        let oswAPI = new OSWApi(configuration);
-        const oswFiles = await oswAPI.listOswFiles();
-        expect(oswFiles.status).toBe(200);
-        expect(Array.isArray(oswFiles.data)).toBe(true);
+  it("Should get list of osw versions", async () => {
+    let oswAPI = new OSWApi(configuration);
 
-    });
+    const oswVersions = await oswAPI.listOswVersions();
 
-    it('Should be able to upload osw files', async ()=>{
-        
-        let metaToUpload = Utility.getRandomOswUpload();
-        // Actual method does not give the results as expected
-        // So we are writing interceptor
+    expect(oswVersions.status).toBe(200);
+    expect(Array.isArray(oswVersions.data.versions)).toBe(true);
+  });
 
-const uploadInterceptor  =  axios.interceptors.request.use((x) => {
-            const data = x.data as FormData;
-            var file = data.get('file') as File;
-            data.delete('file');
-            data.set('file',file,'abc.zip');
-            data.delete('meta');
-            data.set('meta',JSON.stringify(metaToUpload));
-            return x;
-        });
+  it("Should return list of osw files", async () => {
+    let oswAPI = new OSWApi(configuration);
 
-        let fileDir = path.dirname(path.dirname(__dirname));
-        let payloadFilePath = path.join(fileDir,'assets/payloads/osw/files/valid.zip');
-        let filestream = fs.readFileSync(payloadFilePath);
-        const blob = new Blob([filestream],{type:'application/zip'}) ;
-        let oswAPI = new OSWApi(configuration);
-        // try {
-        const uploadedFileResponse = await oswAPI.uploadOswFileForm(metaToUpload,blob);
-        console.log(uploadedFileResponse.data);
-        expect(uploadedFileResponse.status).toBe(202);
-        expect(uploadedFileResponse.data).not.toBe('');
-        axios.interceptors.request.eject(uploadInterceptor);
-        // }
-        // catch (e){
-        //     // console.log(e);
-        //     if (axios.isAxiosError(e)) {
-        //         console.log(e.response?.data);
-        //     }
-        //     axios.interceptors.request.eject(myInterceptor);
-        // }
-    });
-    
+    const oswFiles = await oswAPI.listOswFiles();
 
+    expect(oswFiles.status).toBe(200);
+    expect(Array.isArray(oswFiles.data)).toBe(true);
+  });
 
+  it("Should be able to upload osw files", async () => {
+    let metaToUpload = Utility.getRandomOswUpload();
+    const requestInterceptor = (request, fileName) => {
+      if (
+        request.url === "https://tdei-gateway-dev.azurewebsites.net/api/v1/osw"
+      ) {
+        let data = request.data as FormData;
+        let file = data.get("file") as File;
+        delete data["file"];
+        delete data["meta"];
+        data.set("file", file, fileName);
+        data.set("meta", JSON.stringify(metaToUpload));
+      }
+      return request;
+    };
+    // Actual method does not give the results as expected
+    // So we are writing interceptor
+    const uploadInterceptor = axios.interceptors.request.use((req) =>
+      requestInterceptor(req, "osw-test-upload.zip")
+    );
+    let fileDir = path.dirname(path.dirname(__dirname));
+    let payloadFilePath = path.join(
+      fileDir,
+      "assets/payloads/osw/files/valid.zip"
+    );
+    let filestream = fs.readFileSync(payloadFilePath);
+    const blob = new Blob([filestream], { type: "application/zip" });
+    let oswAPI = new OSWApi(configuration);
+
+    const uploadedFileResponse = await oswAPI.uploadOswFileForm(
+      metaToUpload,
+      blob
+    );
+
+    expect(uploadedFileResponse.status).toBe(202);
+    expect(uploadedFileResponse.data).not.toBeNull();
+    axios.interceptors.request.eject(uploadInterceptor);
+  });
 });
