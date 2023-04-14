2600c6ca2ee9108c20a1d6dc89bed6bf92698996
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-04-14 18:25:57 +0530

    added back

---
+++ src/__tests__/gtfspathways.test.ts
@@ -0,0 +1,47 @@
+import { Configuration, GeneralApi, GTFSPathwaysApi } from "tdei-client";
+import testHarness from "../test-harness.json";
+
+describe('GTFS PATHWAYS API', () => {
+
+    let configuration = new Configuration({
+        username: testHarness.system.username,
+        password: testHarness.system.password,
+        basePath: testHarness.system.baseUrl
+
+    });
+
+    beforeAll(async () => {
+        let generalAPI = new GeneralApi(configuration);
+        const loginResponse = await generalAPI.authenticate({ username: configuration.username, password: configuration.password });
+        configuration.baseOptions = {
+            headers: {
+                'Authorization': 'Bearer ' + loginResponse.data.access_token
+            }
+        };
+    });
+
+    it('Should list GTFS Pathways Files', async () => {
+        let gtfsPathwaysAPI = new GTFSPathwaysApi(configuration);
+        const gtfsPathwaysDownload = await gtfsPathwaysAPI.listPathwaysFiles();
+        expect(Array.isArray(gtfsPathwaysDownload.data)).toBe(true);
+
+    }, 10000)
+
+    it('Should list GTFS Pathways Stations', async () => {
+        let gtfsPathwaysAPI = new GTFSPathwaysApi(configuration);
+        const stations = await gtfsPathwaysAPI.listStations();
+        console.log(stations.data);
+        expect(Array.isArray(stations.data)).toBe(true);
+
+    }, 10000)
+
+    it('Should list GTFS Pathways versions', async () => {
+        let gtfsPathwaysAPI = new GTFSPathwaysApi(configuration);
+        const versions = await gtfsPathwaysAPI.listPathwaysVersions();
+        console.log(versions.data.versions);
+        expect(Array.isArray(versions.data.versions)).toBe(true);
+
+    }, 10000)
+
+
+});
